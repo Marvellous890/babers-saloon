3c9431655e1bab2bd1649848ebb9ebd15c2da3f3
Ajayi Marvellous <ajayimarvellous777@gmail.com>
2023-11-19 07:17:29 +0100

    allowed preview url cors

--- backend/config/allowedOrigin.js
+++ backend/config/allowedOrigin.js
@@ -1,2 +1,2 @@
-const allowedOrigin = ["http://localhost:3000", "http://localhost:5173", "http://127.0.0.1:5173"]
+const allowedOrigin = ["http://localhost:3000", "http://localhost:5173", "http://127.0.0.1:5173", "https://levelup-397803.uc.r.appspot.com"]
 module.exports = allowedOrigin
